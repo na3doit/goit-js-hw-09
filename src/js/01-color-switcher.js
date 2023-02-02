b05fc5d0ba1204e93201e616db80eb3cae911026
Nazar Voitekh <nazar.voitekh@gmail.com>
2023-02-02 22:10:50 +0200

    first_done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,14 +5,16 @@ const body = document.querySelector('body');
 const CHANGE_TIME = 1000;
 let timerId = null;
 
-// startBtn.addEventListener('click', onClickStartBtn);
-// stopBtn.addEventListener('click', onClickStopBtn);
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    const color = getRandomHexColor();
+    body.style.backgroundColor = color;
+  }, CHANGE_TIME);
+});
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+});
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-
-function changeBodyColor() {
-  const color = getRandomHexColor;
-  body.style.backgroundColor = color;
 }

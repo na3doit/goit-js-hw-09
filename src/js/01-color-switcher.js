afebb3993323e4af470affca4114d268027fa5e6
Nazar Voitekh <nazar.voitekh@gmail.com>
2023-02-02 20:24:25 +0200

    add first

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,18 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+const CHANGE_TIME = 1000;
+let timerId = null;
+
+// startBtn.addEventListener('click', onClickStartBtn);
+// stopBtn.addEventListener('click', onClickStopBtn);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBodyColor() {
+  const color = getRandomHexColor;
+  body.style.backgroundColor = color;
+}

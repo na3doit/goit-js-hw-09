dde6e42447ec3314d5915d12fbe7f5925793c32b
na3doit <108612577+na3doit@users.noreply.github.com>
2023-02-07 15:03:39 +0000

    Deploying to gh-pages from @ na3doit/goit-js-hw-09@6c8a10d75fd42cb244eb3b2c6780eb66da520d76 🚀

--- 01-color-switcher.4459f6cc.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=null;t.addEventListener("click",(function(){o=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));n.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(function(){clearInterval(o)}))}();
-//# sourceMappingURL=01-color-switcher.4459f6cc.js.map

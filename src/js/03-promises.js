3dd14c4e2746a6a20808fcbd70b715f99a0ceaf4
Nazar Voitekh <nazar.voitekh@gmail.com>
2023-02-07 18:14:54 +0200

    add_notii

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,4 @@
+import Notiflix from 'notiflix';
 const formEl = document.querySelector('.form');
 formEl.addEventListener('submit', onFormSubmit);
 
@@ -24,10 +25,14 @@ function onFormSubmit(evt) {
   for (let i = 1; i < amount; i += 1) {
     createPromise(i, delay)
       .then(({ position, delay }) => {
-        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
       })
       .catch(({ position, delay }) => {
-        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
       });
     delay += step;
   }
